c126e19d9124625d29f9ff592e4ce392a5aaa4bb
malvasar <marina.marin75.mm@gmail.com>
2022-09-27 17:39:35 -0500

    se crea app react

---
+++ src/Componente/Marina.js
@@ -0,0 +1,12 @@
+export function Marina(){
+    let nombre="Marina"
+    //interpolar=Controlar la GUI desde JS
+
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre}</h1>
+            <hr/>
+
+        </>
+    )
+}
---
+++ src/Sinopsis/Sinopsis.js
@@ -0,0 +1,8 @@
+export function Sinopsis(){
+    return(
+        <>
+            <h1 class="titulo">SINOPSIS</h1>                
+            <p class="sinopsis">Una idea es una representación mental que surge a partir del razonamiento o de la imaginación de una persona. Está considerada como el acto más básico del entendimiento, al contemplar la mera acción de conocer algo. El concepto, de todos modos, tiene varios usos.</p>
+        </>
+    )
+}
